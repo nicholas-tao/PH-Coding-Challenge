c1f11f0793f53a172ab01b1e61d6e9fdb29c897f
nicholas-tao <tao.nicholas1@gmail.com>
2020-06-23 16:14:18 -0400

    add form components + validation, no styling yet

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+//import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import icon from "./images/icon.png";
+import FormComponent from "./FormComponent.js";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Container>
-        <Row>
-          <Col>
-            <div className="img">
-              <img src={icon} className="email-icon" alt="Email Icon" />
-            </div>
-          </Col>
-          <Col>
-            <div className="form">
-              <Form>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Control type="text" placeholder="First Name" />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Control type="email" placeholder="Last Name" />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                  <Form.Control type="email" placeholder="Email address" />
-                </Form.Group>
-
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                  <Form.Control as="textarea" rows="3" placeholder="Message" />
-                </Form.Group>
-                <Button
-                  variant="primary"
-                  type="submit"
-                  className="submit-button"
-                >
-                  Send Email
-                </Button>
-              </Form>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <img src={icon} className="email-icon" alt="Email Icon" />
+          </div>
+          <div className="col">
+            <FormComponent />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

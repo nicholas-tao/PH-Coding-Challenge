620324c91d5f05d054fbe79292cc102e5b30e339
nicholas-tao <tao.nicholas1@gmail.com>
2020-06-23 17:23:34 -0400

    adjust padding in textfields

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import FormComponent from "./FormComponent.js";
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <img src={icon} className="email-icon" alt="Email Icon" />
-          </div>
-          <div className="col form-comp">
-            <FormComponent />
+      <div class="wrapper1">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <img src={icon} className="email-icon" alt="Email Icon" />
+            </div>
+            <div className="col form-comp">
+              <FormComponent />
+            </div>
           </div>
         </div>
       </div>
